886f7a01fc576d2740f6717d3162a9c4c8867f42
kirito178 <108932622+kirito178@users.noreply.github.com>
2022-07-08 19:35:47 +0800

    awa

--- main.go
+++ main.go
@@ -224,7 +224,7 @@ func init() {
 
 	config.W = []*driver.WSClient{driver.NewWebSocketClient(*url, *token)}
 	config.Z = zero.Config{
-		NickName:      append([]string{*adana}, "ATRI", "atri", "亚托莉", "アトリ"),
+		NickName:      append([]string{*adana}, "ATRI", "atri", "亚托莉", ""),
 		CommandPrefix: *prefix,
 		SuperUsers:    sus,
 		Driver:        []zero.Driver{config.W[0]},
